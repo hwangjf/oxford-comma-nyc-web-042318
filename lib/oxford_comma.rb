640a8dc1958207d2ee4be9715e22f343a72260dd
Jeffrey Hwang <jeffreyhwang90@gmail.com>
2018-04-18 00:50:53 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array.first
+  end
+  if array.length == 2
+    return array.first + " and " + array.last
+  end
+  if array.length > 2
+    return array[0...(array.length-1)].join(", ") + ", and " + array.last
+  end
 end
