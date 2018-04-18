0dab0d2d4a7b799a983fadcd31515b1c77d88416
Jeffrey Hwang <jeffreyhwang90@gmail.com>
2018-04-18 00:41:47 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,4 +2,9 @@ def oxford_comma(array)
   if array.length == 1
     return array.first
   end
+  if array.length == 2
+    return array.first + " and " array.last
+  end
+  if array.length > 2
+    return array.join
 end
